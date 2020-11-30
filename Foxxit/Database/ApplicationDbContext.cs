cbd1e194c50ea0bccae4d3b28abd3e60f1699f4b
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2020-11-30 11:45:38 +0100

    init project

---
+++ Foxxit/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foxxit.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // public DbSet<Entity> Entities { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Foxxit/Services/Service.cs
@@ -0,0 +1,19 @@
+﻿using Foxxit.Database;
+using Foxxit.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foxxit.Services
+{
+    public class Service : IService
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+
+        public Service(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
