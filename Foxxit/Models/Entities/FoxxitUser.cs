5ac0712e621007648b1db30c3079962ce3f59b07
Ondrej Sebl <62696517+donkeyjot@users.noreply.github.com>
2020-11-30 16:33:26 +0100

    User model init

--- Foxxit/Models/Entities/FoxxitUser.cs
+++ Foxxit/Models/Entities/FoxxitUser.cs
@@ -1,7 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
 namespace Foxxit.Models.Entities
 {
-    public class FoxxitUser
+    public class FoxxitUser : IdentityUser
     {
-        
+        public string AvatarURL { get; set; }
+        public string DisplayName { get; set; }
+        public int Karma { get; set; }
+        public string About { get; set; }
+        public DateTime MemberSince { get; set; }
     }
 }
