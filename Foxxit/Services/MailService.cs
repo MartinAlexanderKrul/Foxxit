4bb144cfe9812408bccc20246b61dd82b8fe0c67
fakindzej <janwinkler.mu@gmail.com>
2020-12-01 10:30:37 +0100

    add mailservice

---
+++ Foxxit/Services/MailService.cs
@@ -0,0 +1,22 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foxxit.Services
+{
+    public class MailService
+    {
+        public async Task SendEmailAsync(string mailTo, string subject, string content)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("foxxit20020@gmail.com", "Foxxit Team");
+            var to = new EmailAddress(mailTo);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
