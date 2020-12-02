14fcc851f9977fd337474d2a8b6083ed2c166331
fakindzej <janwinkler.mu@gmail.com>
2020-12-02 13:39:09 +0100

    comment out confirmEmailDto

--- Foxxit/Models/DTOs/ConfirmMailDto.cs
+++ Foxxit/Models/DTOs/ConfirmMailDto.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Foxxit.Models.DTOs
-{
-    public class ConfirmMailDto
-    {
-        public string MailTo { get; set; }
-        public string Username { get; set; }
-        public string ConfirmationLink { get; set; }
-        public IDictionary<string, string> CustomMessageHeaders { get; set; }
-    }
-}
+//namespace Foxxit.Models.DTOs
+//{
+//    public class ConfirmMailDto
+//    {
+//        public string MailTo { get; set; }
+//        public string Username { get; set; }
+//        public string ConfirmationLink { get; set; }
+//        public string TemplateId { get; set; }
+
+//        public ConfirmMailDto(string mailTo, string userName, string confirmationLink, string templateId)
+//        {
+//            MailTo = mailTo;
+//            Username = userName;
+//            ConfirmationLink = confirmationLink;
+//            TemplateId = templateId;
+//    }
+//    }
+//}
