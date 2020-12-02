eee2f85ad55715b64fa0a253bfb949419378a0d4
Laszlo Nagy <ltn795@gmail.com>
2020-12-02 15:17:48 +0100

    MIC20 full generic Repo

--- Foxxit/Database/GenericRepository.cs
+++ Foxxit/Database/GenericRepository.cs
@@ -1,48 +1,61 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
+//Exception handling is to be made on the service layer
 namespace Foxxit.Database
 {
     public class GenericRepository<T> where T : class
     {
-        private readonly ApplicationDbContext dbContext;
-        private readonly DbSet<T> table;
+        //dependency injection
+        private readonly ApplicationDbContext dbContext = null;
+        //for being able to define which table to modify or get data from
+        private readonly DbSet<T> table = null;
 
         public GenericRepository()
         {
             dbContext = new ApplicationDbContext();
             table = dbContext.Set<T>();
         }
-        public GenericRepository(EmployeeDBContext _context)
+
+        public GenericRepository(ApplicationDbContext dbContext)
         {
-            this._context = _context;
-            table = _context.Set<T>();
+            this.dbContext = dbContext;
+            table = dbContext.Set<T>();
         }
+
         public IEnumerable<T> GetAll()
         {
             return table.ToList();
         }
+
+        //object id lets us use different id types
         public T GetById(object id)
         {
             return table.Find(id);
         }
+
         public void Insert(T obj)
         {
             table.Add(obj);
         }
+
+        //!!WARNING this one seems a bit different I've used in Java
         public void Update(T obj)
         {
             table.Attach(obj);
-            _context.Entry(obj).State = EntityState.Modified;
+            dbContext.Entry(obj).State = EntityState.Modified;
         }
+
         public void Delete(object id)
         {
             T existing = table.Find(id);
             table.Remove(existing);
         }
+
         public void Save()
         {
-            _context.SaveChanges();
+            dbContext.SaveChanges();
         }
     }
 }
