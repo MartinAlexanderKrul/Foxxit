ca899861d467f3c4abf587f747833bba14e54348
Martin Alexander Krul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2020-12-03 17:37:45 +0100

    Update NetworkCredentials.cs

--- Foxxit/Models/DTO/NetworkCredentials.cs
+++ Foxxit/Models/DTO/NetworkCredentials.cs
@@ -8,13 +8,13 @@ namespace Foxxit.Models.DTO
 {
     public class NetworkCredentials : ICredentialsByHost
     {
-        public NetworkCredential GetCredential(string host, int port, string authenticationType)
-        {
-            throw new NotImplementedException();
-        }
-
         public NetworkCredentials()
         {
+        }
+        
+        public NetworkCredential GetCredential(string host, int port, string authenticationType)
+        {
+            return new NetworkCredential();
         }
     }
 }
