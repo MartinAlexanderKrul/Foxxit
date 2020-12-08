dde5f5075f9bc2b4a36e3ea5589ec4eba84b17b6
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2020-12-08 12:28:04 +0100

    add first look for main view

---
+++ Foxxit/Models/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foxxit.Models.Entities;
+
+namespace Foxxit.Models.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+        }
+
+        public List<Post> Posts { get; set; }
+    }
+}
