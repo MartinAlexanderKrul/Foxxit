e2c5fcaeeeeef8764e0635ce8dc892d402158603
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2020-12-08 13:24:07 +0100

    add postservice methods

--- Foxxit/Services/PostService.cs
+++ Foxxit/Services/PostService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Foxxit.Models.Entities;
 using Foxxit.Repositories;
 
 namespace Foxxit.Services
@@ -14,5 +15,35 @@ namespace Foxxit.Services
         }
 
         public PostRepository PostRepository { get; private set; }
+
+        public async Task<IEnumerable<Post>> GetAllAsync()
+        {
+            return await PostRepository.GetAllAsync();
+        }
+
+        public async Task<Post> GetByIdAsync(long id)
+        {
+            return await PostRepository.GetByIdAsync(id);
+        }
+
+        public void Add(Post obj)
+        {
+            PostRepository.AddAsync(obj);
+        }
+
+        public void Update(Post obj)
+        {
+            PostRepository.Update(obj);
+        }
+
+        public void Delete(Post obj)
+        {
+            PostRepository.Delete(obj);
+        }
+
+        public void Save()
+        {
+            PostRepository.SaveAsync();
+        }
     }
 }
