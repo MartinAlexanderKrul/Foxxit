fbc578e4ed1f4c538c652a8188d95412adbc594a
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2020-12-11 11:36:47 +0100

    fix bugs

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -18,7 +18,25 @@ namespace Foxxit.Controllers
         [HttpGet("")]
         public async Task<IActionResult> Index()
         {
-            var model = new MainPageViewModel();
+            var user = new User("Nicolsburg");
+            var subreddits = new List<SubReddit>
+            {
+                new SubReddit() { Name = "Green Fox", Id = 1 },
+                new SubReddit() { Name = "Microtis", Id = 2 },
+                new SubReddit() { Name = "Sageeeee", Id = 3 },
+                new SubReddit() { Name = "Vulpes", Id = 9 },
+            };
+            var posts = new List<Post>()
+            {
+                new Post() { User = user,  Id = 1,  SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
+                new Post() { User = user, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
+            };
+            var model = new MainPageViewModel()
+            {
+                CurrentUser = user,
+                SubReddits = subreddits,
+                Posts = posts,
+            };
             return await Task.Run(() => View("Index", model));
         }
     }
