f2a53b7a5789ca637ae1596c0372817f8ee438a0
fakindzej <janwinkler.mu@gmail.com>
2020-12-14 10:07:43 +0100

    init

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -1,18 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foxxit.Models.Entities;
+using Foxxit.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Foxxit.Controllers
 {
     public class FoxxitController : Controller
     {
-        // public Service Service { get; private set; }
-        public FoxxitController(/*Service service*/)
+        private readonly UserService userService;
+        private readonly PostService postService;
+
+        public FoxxitController(UserService userService, PostService postService)
         {
+            this.userService = userService;
+            this.postService = postService;
         }
 
         [HttpGet("index")]
         public IActionResult Index()
         {
             return View("index");
+        }
+
+        [HttpGet("paginationTest")]
+        public async Task<IActionResult> PaginationTest(int? pageNum)
+        {
+            var posts = await postService.GetAllAsync();
+            int pageSize = 10;
+
+            return View(await PaginatedList<Post>.CreateAsync(posts, pageNum ?? 1, pageSize));
         }
     }
 }
