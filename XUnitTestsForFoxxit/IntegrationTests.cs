5298241783c235b0c3449309f525350682e0ffdc
fakindzej <janwinkler.mu@gmail.com>
2020-12-14 22:30:24 +0100

    add db startup

--- XUnitTestsForFoxxit/IntegrationTests.cs
+++ XUnitTestsForFoxxit/IntegrationTests.cs
@@ -1,42 +1,53 @@
-﻿//using Foxxit;
-//using Microsoft.AspNetCore.Mvc.Testing;
-//using Newtonsoft.Json;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Foxxit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace XUnitTestsForFoxxit
-//{
-//    public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
-//    {
-//        private readonly WebApplicationFactory<Startup> factory;
+namespace XUnitTestsForFoxxit
+{
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> factory;
 
-//        public IntegrationTests(WebApplicationFactory<Startup> factory)
-//        {
-//            this.factory = factory;
-//        }
+        public IntegrationTests(WebApplicationFactory<Startup> factory)
+        {
+            Environment.SetEnvironmentVariable("DbType", "InMemory");
 
-//        #region Sample Test
-//        [Fact]
-//        public async Task Doubling_ReturnsResult()
-//        {
-//            //Arrange
-//            var expected = 10;
-//            var response = await factory.CreateClient().GetAsync("doubling?input=5");
-//            var data = await response.Content.ReadAsStringAsync();
+            this.factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.UseSolutionRelativeContentRoot("Foxxit");
 
-//            //Act
-//            var result = JsonConvert.DeserializeObject<Dictionary<string, int>>(data);
-//            result.TryGetValue("result", out int actual);
+                //builder.ConfigureServices(services =>
+                //{
+                //    services.AddMvc().AddApplicationPart(typeof(Startup).Assembly);
+                //});
+            });
+        }
 
-//            //Assert
-//            response.EnsureSuccessStatusCode();
-//            Assert.Equal(expected, actual);
-//        }
-//        #endregion
+        #region Sample Test
+        [Fact]
+        public async Task Doubling_ReturnsResult()
+        {
+            //Arrange
+            var expected = 10;
+            var response = await factory.CreateClient().GetAsync("doubling?input=5");
+            var data = await response.Content.ReadAsStringAsync();
 
-//    }
-//}
+            //Act
+            var result = JsonConvert.DeserializeObject<Dictionary<string, int>>(data);
+            result.TryGetValue("result", out int actual);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+    }
+}
