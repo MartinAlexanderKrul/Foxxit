10f25f8f80c479498ca0f49a0c616419dc85a1e9
fakindzej <janwinkler.mu@gmail.com>
2020-12-14 11:00:40 +0100

    comment out tests and add a circle ci config

--- XUnitTestsForFoxxit/UnitTests.cs
+++ XUnitTestsForFoxxit/UnitTests.cs
@@ -1,21 +1,21 @@
-using Xunit;
+//using Xunit;
 
-namespace XUnitTestsForFoxxit
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test()
-        {
-            ////arrange
-            //var apple = new Apple();
+//namespace XUnitTestsForFoxxit
+//{
+//    public class UnitTest1
+//    {
+//        [Fact]
+//        public void Test()
+//        {
+//            ////arrange
+//            //var apple = new Apple();
 
-            ////act
-            //string actualOutput = apple.GetApple();
-            //string expectedOutput = "apple";
+//            ////act
+//            //string actualOutput = apple.GetApple();
+//            //string expectedOutput = "apple";
 
-            ////assert
-            //Assert.Equal(actualOutput, expectedOutput);
-        }
-    }
-}
+//            ////assert
+//            //Assert.Equal(actualOutput, expectedOutput);
+//        }
+//    }
+//}
