9bb4482ccd65279aff06233cad0c1179d87b9426
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2021-01-04 11:52:13 +0100

    delete javascript

--- Foxxit/wwwroot/js/JavaScript.js
+++ Foxxit/wwwroot/js/JavaScript.js
@@ -1,8 +1,1 @@
-﻿function hideComments() {
-    var x = document.getElementById("comment_container");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+﻿
