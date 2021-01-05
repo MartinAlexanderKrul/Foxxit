af0eb6e8c1e31a2f07d42c295b0eedf12d3f79e5
fakindzej <janwinkler.mu@gmail.com>
2021-01-05 17:52:47 +0100

    fix css

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -79,7 +79,7 @@ namespace Foxxit.Controllers
             };
             var posts = new List<Post>()
             {
-                new Post() { User = currentUser,  Id = 1,  SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
+                new Post() { User = currentUser,  Id = 1,  SubReddit = new SubReddit() { Name = "Greetgtgtgtgtn Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
                 new Post() { User = currentUser, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
             };
             var subReddit = new SubReddit() { Name = "Green Fox", Id = 1, Posts = posts };
