0ddb3123e6f4c6d7e4b107552bed98161eaff101
fakindzej <janwinkler.mu@gmail.com>
2021-01-05 18:07:10 +0100

    remove dummy objects

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -65,25 +65,10 @@ namespace Foxxit.Controllers
         [HttpGet("subreddit/{subRedditId}")]
         public async Task<IActionResult> Subreddit([FromRoute]int subRedditId)
         {
-            //var currentUser = await GetActiveUserAsync();
-            //var subReddit = await SubRedditService.GetByIdAsync(subRedditId);
-            //var subReddits = await SubRedditService.GetAllAsync();
-
-            var currentUser = new User("Nicolsburg", "nicolsburg@hocz.org");
-            var subReddits = new List<SubReddit>
-            {
-                new SubReddit() { Name = "Green Fox", Id = 1 },
-                new SubReddit() { Name = "Microtis", Id = 2 },
-                new SubReddit() { Name = "Sageeeee", Id = 3 },
-                new SubReddit() { Name = "Vulpes", Id = 9 },
-            };
-            var posts = new List<Post>()
-            {
-                new Post() { User = currentUser,  Id = 1,  SubReddit = new SubReddit() { Name = "Greetgtgtgtgtn Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
-                new Post() { User = currentUser, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
-            };
-            var subReddit = new SubReddit() { Name = "Green Fox", Id = 1, Posts = posts };
-
+            var currentUser = await GetActiveUserAsync();
+            var subReddit = await SubRedditService.GetByIdAsync(subRedditId);
+            var subReddits = await SubRedditService.GetAllAsync();
+            
             var model = new SubRedditViewModel()
             {
                 User = currentUser,
