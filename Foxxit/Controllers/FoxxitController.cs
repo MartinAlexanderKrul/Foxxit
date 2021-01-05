4ca4c9564d8911256aa1123d54638e5dd29b6103
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2021-01-05 14:13:51 +0100

    jQuery add

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Foxxit.Models.DTO;
 using Foxxit.Models.Entities;
 using Foxxit.Models.ViewModels;
@@ -28,14 +29,59 @@ namespace Foxxit.Controllers
         [HttpGet("")]
         public async Task<IActionResult> Index()
         {
-            var model = new MainPageViewModel()
+            var user = new User("Nicolsburg", "nicolsburg@hocz.org");
+
+            var subreddits = new List<SubReddit>
             {
-                // CurrentUser = await GetActiveUserAsync(),
-                Posts = await PostService.GetAllAsync(),
-                SubReddits = await SubRedditService.GetAllAsync(),
+                new SubReddit() { Name = "Green Fox", Id = 1 },
+                new SubReddit() { Name = "Microtis", Id = 2 },
+                new SubReddit() { Name = "Sageeeee", Id = 3 },
+                new SubReddit() { Name = "Vulpes", Id = 9 },
             };
 
-            return View("Index", model);
+            var subComments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first SUBcomment of all comments.",
+            },
+                new Comment()
+                {
+                    User = user,
+                    Text = "This is the second one. SUB!",
+                },
+            };
+
+            var comments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first comment of all comments.",
+            },
+                new Comment()
+                {
+                    Comments = subComments,
+                    User = user,
+                    Text = "This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment.",
+                },
+            };
+
+            var posts = new List<Post>()
+            {
+                new Post() { Comments = comments, User = user,  Id = 1,  SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
+                new Post() { User = user, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
+            };
+
+            var model = new MainPageViewModel()
+            {
+                CurrentUser = user,
+                SubReddits = subreddits,
+                Posts = posts,
+            };
+
+            return await Task.Run(() => View("Index", model));
         }
 
         [HttpPost("search")]
@@ -58,6 +104,61 @@ namespace Foxxit.Controllers
             var posts = await PostService.GetAllAsync();
 
             return View(await PaginatedList<Post>.CreateAsync(posts, pageNum ?? 1, PageSize));
+        }
+
+        [HttpGet("loadComments")]
+        public async Task<IActionResult> LoadComments(int postId)
+        {
+            var model = await PostService.GetByIdAsync(postId);
+
+            var user = new User("Nicolsburg", "nicolsburg@hocz.org");
+
+            var subreddits = new List<SubReddit>
+            {
+                new SubReddit() { Name = "Green Fox", Id = 1 },
+                new SubReddit() { Name = "Microtis", Id = 2 },
+                new SubReddit() { Name = "Sageeeee", Id = 3 },
+                new SubReddit() { Name = "Vulpes", Id = 9 },
+            };
+
+            var subComments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first SUBcomment of all comments.",
+            },
+                new Comment()
+                {
+                    User = user,
+                    Text = "This is the second one. SUB!",
+                },
+            };
+
+            var comments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first comment of all comments.",
+            },
+                new Comment()
+                {
+                    Comments = subComments,
+                    User = user,
+                    Text = "This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment.",
+                },
+            };
+
+            var posts = new List<Post>()
+            {
+                new Post() { Comments = comments, User = user,  Id = 1,  SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
+                new Post() { User = user, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
+            };
+
+            var dummyModel = new Post() { Comments = comments, User = user, Id = 1, SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " };
+
+            return PartialView("_CommentViewPartial", dummyModel);
         }
     }
 }
--- Foxxit/wwwroot/js/JavaScript.js
+++ Foxxit/wwwroot/js/JavaScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿$('.load-comments').on('click', function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    if (this !== 'open') {
+        $('#comment_container_js').load("/loadComments");
+        $('.closed').toggleClass("open");
+    } else {
+        $('open').toggleClass("closed");
+        $('#comment_container_js').load('');
+    }
+});
