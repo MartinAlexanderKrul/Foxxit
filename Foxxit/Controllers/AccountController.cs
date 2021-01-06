ce863f58031a77c1f9ff17bc89726d3c2e036b16
bouraccino <ondrejmach.mail@gmail.com>
2021-01-06 12:07:54 +0100

    ImageStore-InitMigration-UpdatedViews

--- Foxxit/Controllers/AccountController.cs
+++ Foxxit/Controllers/AccountController.cs
@@ -170,7 +170,7 @@ namespace Foxxit.Controllers
         [HttpGet("external-login")]
         public async Task<IActionResult> ExternalLoginCallback(User user = null, string returnUrl = null, string remoteError = null)
         {
-            returnUrl ??= "/foxxit/index";
+            returnUrl ??= "/index";
             var externalInfo = await SignInManager.GetExternalLoginInfoAsync();
 
             if (externalInfo is null)
