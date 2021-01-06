353ad71c2a91ab8f880b5e77f2e1478bb49a6639
fakindzej <janwinkler.mu@gmail.com>
2021-01-06 17:47:03 +0100

    add controller

---
+++ Foxxit/Controllers/VoteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foxxit.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foxxit.Controllers
+{
+    public class VoteController : Controller
+    {
+        public VoteController(IVoteService voteService)
+        {
+            VoteService = voteService;
+        }
+
+        public IVoteService VoteService { get; set; }
+
+        [HttpPost("vote")]
+        public async Task<IActionResult> Vote(int voteType, int postId)
+        {
+            
+        }
+    }
+}
