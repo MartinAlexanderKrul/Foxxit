4769f9554cbdae07c35921920a529acd7106aac8
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2021-01-06 14:21:25 +0100

    forgot bracet

--- Foxxit/Controllers/FoxxitController.cs
+++ Foxxit/Controllers/FoxxitController.cs
@@ -29,14 +29,59 @@ namespace Foxxit.Controllers
         [HttpGet("")]
         public async Task<IActionResult> Index()
         {
-            var model = new MainPageViewModel()
+            var user = new User("Nicolsburg", "nicolsburg@hocz.org");
+
+            var subreddits = new List<SubReddit>
             {
-                // CurrentUser = await GetActiveUserAsync(),
-                Posts = await PostService.GetAllAsync(),
-                SubReddits = await SubRedditService.GetAllAsync(),
+                new SubReddit() { Name = "Green Fox", Id = 1 },
+                new SubReddit() { Name = "Microtis", Id = 2 },
+                new SubReddit() { Name = "Sageeeee", Id = 3 },
+                new SubReddit() { Name = "Vulpes", Id = 9 },
             };
 
-            return View("Index", model);
+            var subComments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first SUBcomment of all comments.",
+            },
+                new Comment()
+                {
+                    User = user,
+                    Text = "This is the second one. SUB!",
+                },
+            };
+
+            var comments = new List<Comment>()
+            {
+                new Comment()
+            {
+                    User = user,
+                    Text = "This is the first comment of all comments.",
+            },
+                new Comment()
+                {
+                    Comments = subComments,
+                    User = user,
+                    Text = "This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment. This is the second comment.",
+                },
+            };
+
+            var posts = new List<Post>()
+            {
+                new Post() { Comments = comments, User = user,  Id = 1,  SubReddit = new SubReddit() { Name = "Green Fox", Id = 4 }, Title = "Green Fox", Text = "fwafawfajwfjawifjawkjfkawfnkjawh faw jakwfj kawfjj kawf jkawhf jkawhnfk " },
+                new Post() { User = user, Id = 2, SubReddit = new SubReddit() { Name = "Green Fox", Id = 5 }, Title = "Green Fox", ImageURL = "https://www.spacesworks.com/wp-content/uploads/2016/06/coding-in-the-classroom.png" },
+            };
+
+            var model = new MainPageViewModel()
+            {
+                CurrentUser = user,
+                SubReddits = subreddits,
+                Posts = posts,
+            };
+
+            return await Task.Run(() => View("Index", model));
         }
 
         [HttpPost("search")]
@@ -44,7 +89,7 @@ namespace Foxxit.Controllers
         {
             var model = new MainPageViewModel()
             {
-                // CurrentUser = await GetActiveUserAsync(),
+                CurrentUser = await GetActiveUserAsync(),
                 Posts = await PostService.GetAllAsync(),
                 SubReddits = await SubRedditService.GetAllAsync(),
                 SearchReturnModel = SearchService.Search(category, keyword),
@@ -66,7 +111,7 @@ namespace Foxxit.Controllers
         {
             var model = new MainPageViewModel()
             {
-                // CurrentUser = await GetActiveUserAsync(),
+                CurrentUser = await GetActiveUserAsync(),
                 SubReddits = await SubRedditService.GetAllAsync(),
                 CurrentSubReddit = await SubRedditService.GetByIdAsync(subRedditId),
             };
