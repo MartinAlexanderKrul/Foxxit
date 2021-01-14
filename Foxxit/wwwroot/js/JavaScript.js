986d2afe0d2bf35f576f02bb3db8e601a80c82bd
fakindzej <janwinkler.mu@gmail.com>
2021-01-14 15:33:01 +0100

    add js code for vote

--- Foxxit/wwwroot/js/JavaScript.js
+++ Foxxit/wwwroot/js/JavaScript.js
@@ -11,3 +11,20 @@
 
 //    $('#addcomment').load("/newComment");
 //});
+
+$(document).on('click', '#upvote-btn', function (event) {
+    $.ajax({
+        url: '/vote',
+        type: "post",
+        contentType: 'application/json;charset=UTF-8',
+        dataType: "json",
+        data: JSON.stringify({ 'postBaseId': $('#vote-btn').data('postBaseId') }),
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (xhr) {
+            console.log(xhr);
+        }
+    });
+    event.preventDefault();
+});
