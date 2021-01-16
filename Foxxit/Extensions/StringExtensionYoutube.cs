27a7d34264bc3736cb52720d6390c202dc90dd32
MartinAlexanderKrul <68639677+MartinAlexanderKrul@users.noreply.github.com>
2021-01-16 19:34:31 +0100

    make youtube funcionality and divide PostPartial into two partial views

---
+++ Foxxit/Extensions/StringExtensionYoutube.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Foxxit.Extensions
+{
+    public static class YoutubeExtension
+    {
+        public static string GetYoutubeId(this string url)
+        {
+            var uri = new Uri(url);
+            var query = HttpUtility.ParseQueryString(uri.Query);
+
+            if (query.AllKeys.Contains("v"))
+            {
+                return query["v"];
+            }
+
+            return uri.Segments.Last();
+        }
+    }
+}
