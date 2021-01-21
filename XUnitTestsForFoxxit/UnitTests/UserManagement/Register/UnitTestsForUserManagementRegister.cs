351aaef47f093fa6c3867e6eebe9536f9fa99292
fakindzej <janwinkler.mu@gmail.com>
2021-01-21 14:36:26 +0100

    shorten the vote button conditions

--- XUnitTestsForFoxxit/UnitTests/UserManagement/Register/UnitTestsForUserManagementRegister.cs
+++ XUnitTestsForFoxxit/UnitTests/UserManagement/Register/UnitTestsForUserManagementRegister.cs
@@ -3,7 +3,7 @@ using Foxxit.Models.ViewModels;
 using Foxxit.Controllers;
 using Moq;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations; // whitespace fix
 using System.Linq;
 
 namespace XUnitTestsForFoxxit
